92ee138bfa7a634f9042664be81d9f47f5953cc4
DonRobot-all <63363659+DonRobot-all@users.noreply.github.com>
2022-06-14 17:28:39 +0300

    Update greenhouse.ino
    работа Максима

--- greenhouse.ino
+++ greenhouse.ino
@@ -1,9 +1,40 @@
+//light_sensor-датчик света
+//temperature_sensor-датчик температуры
+//humidity_sensor-датчик влажности
+//water_level_sensor-датчик уровня воды
+//timer-таймер
+//UV_lamp-УФ лампа
+#define light_sensor A0
+#define temperature_sensor A1
+#define humidity_sensor 2
+#define water_level_sensor A2
+#define timer 3
+#define UV_lamp 7
+
 void setup() {
-  pinMode(light, OUTPUT);
+  pinMode(light_sensor, OUTPUT);
+  pinMode(temperature_sensor, OUTPUT);
+  pinMode(humidity_sensor, OUTPUT);
+  pinMode(water_level_sensor, OUTPUT);
+  pinMode(timer, OUTPUT);
+  pinMode(UV_lamp, OUTPUT);
+  Serial.begin(9600);
+}
+
+
+
+void loop(){
+light();
+delay(1000);
+temperature();
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void light() {
+  Serial.println(analogRead(A0));
+}
+
+void temperature() {
+  Serial.println(analogRead(A1));
 }
